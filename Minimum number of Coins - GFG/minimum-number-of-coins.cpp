dd113eb22794b8e84e8f067f915480e15571e3ab
Parichita Das <60207676+PerryAgent@users.noreply.github.com>
2022-07-12 19:22:49 +0530

    Added solution - LeetHub

--- Minimum number of Coins - GFG/minimum-number-of-coins.cpp
+++ Minimum number of Coins - GFG/minimum-number-of-coins.cpp
@@ -10,30 +10,69 @@ using namespace std;
 class Solution{
 public:
 
-    vector<int>den = {2000, 500, 200, 100, 50, 20, 10, 5, 2, 1};
+    vector<int>den = {1, 2, 5, 10, 20, 50, 100, 200, 500, 2000};
     
     vector<int> minPartition(int N)
     {
         // code here
-        vector<int>change;
-        while(N>0) {
-            for(auto i: den){
-                if(i <= N) {
-                    for(int j=0; j<N/i; j++)
-                        change.push_back(i);
-                    N = N%i;
-                }
-            }
-        }
+        vector<vector<int>>change(den.size(), vector<int>(N+1));
+        // while(N>0) {
+        //     for(auto i: den){
+        //         if(i <= N) {
+        //             for(int j=0; j<N/i; j++)
+        //                 change.push_back(i);
+        //             N = N%i;
+        //         }
+        //     }
+        // }
         /*
         11
         7 6 5 1
         7
         1 1 1 1
         
+        0 1 2 3 4 5 6 7 8 9 10 11
+    1   0 1 2 3 4 5 6 7 8 9 10 11
+    5   0 1 2 3 4 1 2 3 4 5 2  3
+    6   0 1 2 3 4 1 1 2 3 4 5  2
+    7   0 1 2 3 4 1 1 1 2 3 4  5
+    
+        
         */
         
-        return change;
+        for(int i=0; i<den.size(); i++) {
+            change[i][0] = 0;
+        }
+        
+        for(int i=0; i<=N; i++) {
+            change[0][i] = i;
+        }
+        
+        for(int i=1; i<den.size(); i++) {
+            for(int j=1; j<=N; j++) {
+                change[i][j] = (j/den[i]) + change[i-1][j%den[i]];
+            }
+        }
+        
+        vector<int>ans;
+        int im;
+        int mn = INT_MAX;
+        for(int i=0; i<den.size(); i++) {
+        //  mn = min(mn, );  
+         if (mn > change[i][N]){
+             mn = change[i][N];
+             im = i;
+         }
+        }
+        
+        while(N>0) {
+            for(int i=0; i<N/den[im]; i++)
+                ans.push_back(den[im]);
+            N = N%den[im];
+            im--;
+        }
+        
+        return ans;
     }
 };
 
