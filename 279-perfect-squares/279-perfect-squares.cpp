1ced3177702d75bcb889cae39a638118aba145f0
Parichita Das <60207676+PerryAgent@users.noreply.github.com>
2022-08-18 13:29:00 +0530

    Time: 1172 ms (6.45%), Space: 152.3 MB (7.29%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.cpp
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     
-    int findLeast(vector<int>& psq, int i, int s, vector<vector<int>>&m)
+    long long findLeast(vector<int>& psq, int i, int s, vector<vector<int>>&m)
     {
         if(s==0)
             return 0;
-        if(i==psq.size()) return (int) 1e5;
+        if(i==psq.size()) return INT_MAX;
         
-        if(psq[i] > s) return (int) 1e5;
+        if(psq[i] > s) return INT_MAX;
         if(m[i][s] != -1)
             return m[i][s];
-        return m[i][s] = min(1+findLeast(psq, i, s-psq[i], m), findLeast(psq, i+1, s, m));
+        return m[i][s] = min(1ll+findLeast(psq, i, s-psq[i], m), findLeast(psq, i+1, s, m));
     }
     int numSquares(int n) {
         vector<int>psq;
-        for(int i=1; i*i<=n; i++) {
+        for(int i=1; i<=sqrt(n); i++) {
             psq.push_back(i*i);
         }
         vector<vector<int>>m(psq.size(), vector<int>(n+1, -1));
-        int ans = findLeast(psq, 0, n, m);
+        long long ans = findLeast(psq, 0, n, m);
         return ans;
     }
 };
